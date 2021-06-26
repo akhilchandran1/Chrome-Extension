7a6aa66f23fd2a1e943b690223cb3f9b30ad7bd6
Akhil Chandran <akhil9216@gmail.com>
2021-06-26 21:16:22 +0100

    create frontend

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let myWebPages = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
+
+
+tabBtn.addEventListener("click", function(){
+
+})
+
+function render(WebURL){
+
+}
+
+deleteBtn.addEventListener("click", function(){
+
+})
+
+inputBtn.addEventListener("click", function(){
+
+})
